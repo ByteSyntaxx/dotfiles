efe9dc48d99918e5d87f8e5889fdeb9a1c1fcd0c
ByteSyntaxx <95436329+ByteSyntaxx@users.noreply.github.com>
2023-12-16 11:40:27 +0100

    Updated Dotfiles

--- conflinks.sh
+++ conflinks.sh
@@ -2,17 +2,6 @@
 
 # link all configs via symlink
 
-# zsh plugins
-ln -s ~/Documents/dotfiles/.zsh/plugins/ ~/.zsh
-
-# zsh configs
-ln -s ~/Documents/dotfiles/.zsh/.zshrc ~/.zshrc
-ln -s ~/Documents/dotfiles/.zsh/.zshenv ~/.zshenv
-ln -s ~/Documents/dotfiles/.zsh/.zlogin ~/.zlogin
-
-# starship config
-ln -s ~/Documents/dotfiles/.config/starship.toml ~/.config/starship.toml
-
 # program configs
 ln -s ~/Documents/dotfiles/.config/fish/ ~/.config
 ln -s ~/Documents/dotfiles/.config/hypr/ ~/.config
