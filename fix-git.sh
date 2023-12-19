2d1c33ea7fdf9464159c18d8bf82ffee05b523f7
ByteSyntaxx <95436329+ByteSyntaxx@users.noreply.github.com>
2023-12-19 17:31:52 +0100

    Delete fix-git.sh

--- fix-git.sh
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-#!/bin/bash
-
-git config --global user.email "95436329+ByteSyntaxx@users.noreply.github.com"
-git config --global user.name "ByteSyntaxx"
-git config --global pull.rebase false
-=======
->>>>>>> 5033a95da250e85aa706a9a8c9ef8f01c91224ca
-
