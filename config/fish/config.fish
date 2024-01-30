31eea05728005584ba74a99bda8e274ca2078a31
ByteSyntaxx <95436329+ByteSyntaxx@users.noreply.github.com>
2024-01-30 17:31:28 +0100

    Updated Dotfiles

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,10 +3,5 @@ set fish_greeting
  
 export EDITOR=nvim
 
-#set fish_color_param '#D3869B'
-#set fish_color_error '#E26469'
-#set fish_color_command '#458588'
-#set fish_color_autosuggestion '#969696'
-
 fish_aliases
 neofetch
