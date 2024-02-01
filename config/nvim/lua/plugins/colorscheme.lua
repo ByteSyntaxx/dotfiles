085d6bea4e08b1f2c4f0e4f2cf0dd773d9ea7caf
ByteSyntaxx <95436329+ByteSyntaxx@users.noreply.github.com>
2024-02-01 17:21:04 +0100

    Updated Dotfiles

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
-    {'navarasu/onedark.nvim'},
-    {'catppuccin/nvim'},
-    {'projekt0n/github-nvim-theme'},
-    {'EdenEast/nightfox.nvim'},
-    {'navarasu/onedark.nvim'},
-    {'maxmx03/solarized.nvim'},
-    {'ellisonleao/gruvbox.nvim'}
+--    {'navarasu/onedark.nvim'},
+--    {'catppuccin/nvim'},
+--    {'projekt0n/github-nvim-theme'},
+--    {'EdenEast/nightfox.nvim'},
+--    {'navarasu/onedark.nvim'},
+--    {'maxmx03/solarized.nvim'},
+--    {'ellisonleao/gruvbox.nvim'}
 }
