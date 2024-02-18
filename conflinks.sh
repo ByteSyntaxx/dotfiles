5bc2e2c77ad9de43702268d13d9fa2176fa0db3c
ByteSyntaxx <95436329+ByteSyntaxx@users.noreply.github.com>
2024-02-18 16:39:03 +0100

    Updated Dotfiles

--- conflinks.sh
+++ conflinks.sh
@@ -3,12 +3,7 @@
 # link all configs via symlink
 
 # program configs
-ln -s ~/Documents/dotfiles/config/fish/ ~/.config
-ln -s ~/Documents/dotfiles/config/hypr/ ~/.config
-ln -s ~/Documents/dotfiles/config/nvim/ ~/.config
-ln -s ~/Documents/dotfiles/config/wofi/ ~/.config
-ln -s ~/Documents/dotfiles/config/kitty/ ~/.config
-ln -s ~/Documents/dotfiles/config/neofetch/ ~/.config
+ln -s ~/Documents/dotfiles/config/* ~/.config/
 
 # wallpaper
 ln -s ~/Documents/dotfiles/wallpaper/ ~/Pictures
