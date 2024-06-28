434a088f2990b4342b81ff181e4fca0e5bb3aa37
ByteSyntaxx <95436329+ByteSyntaxx@users.noreply.github.com>
2024-06-28 20:38:20 +0200

    Updates Dotfiles `2024-06-28 20:38:20`

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -2,7 +2,7 @@ return {
     {'nvim-lualine/lualine.nvim', requires={'nvim-tree/nvim-web-devicons'},
         config=function()
             require('lualine').setup({
-                options={theme='everforest'}
+                options={theme='catppuccin'}
             })
         end
     }
