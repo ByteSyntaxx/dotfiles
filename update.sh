6d94cf426fc81e91ce2f7b260480bfa80861effa
ByteSyntaxx <95436329+ByteSyntaxx@users.noreply.github.com>
2024-09-14 17:46:11 +0200

    Updated Dotfiles 2024-09-14 / 17:46:&S

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Make sure that local dotfiles are up to date 
+git pull
+
+# Date and Time timestamps
+
+date=`/bin/date '+%Y-%m-%d'`
+time=`/bin/date '+%H:%M:&S'`
+
+# push to github
+git add .
+git commit -m "Updated Dotfiles ${date} / ${time}"
+git push
