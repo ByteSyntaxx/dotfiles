1fd6ce6b7305f1eb41bcd0c9d2aa707b2373425a
ByteSyntaxx <95436329+ByteSyntaxx@users.noreply.github.com>
2025-05-03 13:21:29 +0200

    Updated Dotfiles 2025-05-03 13:21

--- config/hypr/scripts/startup.sh
+++ config/hypr/scripts/startup.sh
@@ -5,6 +5,5 @@ SCRIPTSDIR=$HOME/.config/hypr/scripts
 # Apps
 "${SCRIPTSDIR}"/desktop-portal &
 "${SCRIPTSDIR}"/xauthority &
-waybar & hyprpaper & mako &
 systemctl start tor --now &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
