08a4ac8d9055bf70b4e8dd43d2ea12db7213331b
ByteSyntaxx <95436329+ByteSyntaxx@users.noreply.github.com>
2025-05-14 17:36:32 +0200

    Updated Dotfiles 2025-05-14 17:36

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # install window manager & terminal
-sudo pacman -S --noconfirm --needed hyprland hyprpaper hyprlock xdg-desktop-portal-hyprland kitty kitty-terminfo kitty-shell-integration qt5-wayland qt6-wayland polkit-gnome
+sudo pacman -S --noconfirm --needed hyprland hyprpaper hyprlock xdg-desktop-portal-hyprland kitty qt5-wayland qt6-wayland polkit-gnome
 
 # install basic programs
-sudo pacman -S --noconfirm --needed waybar fuzzel mako cliphist obsidian discord bitwarden pcmanfm intellij-idea-community-edition
+sudo pacman -S --noconfirm --needed fish waybar fuzzel mako cliphist obsidian discord bitwarden pcmanfm intellij-idea-community-edition
+
+# install fonts
+sudo pacman -S --noconfirm --needed ttf-firacode-nerd ttf-jetbrains-mono ttf-roboto noto-fonts noto-fonts-extra noto-fonts-emoji
 
 # install paru / aur helper
 sudo pacman -S --needed base-devel
@@ -12,4 +15,6 @@ git clone https://aur.archlinux.org/paru.git
 cd paru || exit
 makepkg -si
 
-paru -S --noconfirm --needed brave-bin visual-studio-code-bin sysmontask
+paru -S --noconfirm --needed brave-bin visual-studio-code-bin sysmontask nwg-look
+
+sudo chsh bytesyntaxx -s /bin/fish
