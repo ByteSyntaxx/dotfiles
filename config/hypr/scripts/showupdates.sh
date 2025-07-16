737ee86d7024d1aa38267a48bf8430cb74753622
ByteSyntaxx <95436329+ByteSyntaxx@users.noreply.github.com>
2025-07-16 13:42:42 +0200

    Updated Dotfiles 2025-07-16 13:42

--- config/hypr/scripts/showupdates.sh
+++ config/hypr/scripts/showupdates.sh
@@ -1,1 +1,10 @@
-notify-send "It's Updatetime" "There are $(checkupdates|count) updates available." --icon ~/.config/mako/notify.png
+#!/bin/bash
+
+updates=$(/bin/checkupdates|wc -l);
+
+if [ $updates -gt 0 ] 2> /dev/null
+then
+    notify-send "It's Update Time" "There are $updates updates available." --icon ~/.config/mako/notify.png;    
+else
+    exit 0
+fi
