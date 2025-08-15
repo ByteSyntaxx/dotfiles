746713ed5e4f5160f89f216966a841d4117c7752
ByteSyntaxx <95436329+ByteSyntaxx@users.noreply.github.com>
2025-08-15 13:43:39 +0200

    Updated Dotfiles 2025-08-15 13:43

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,13 +1,9 @@
 ## CHANGE SOME FISH SETTINGS ##
 set fish_greeting ""
 
+export EDITOR=vim
+
 fish_add_path /home/bytesyntaxx/.spicetify
 
-export EDITOR=nvim
-export PF_INFO="ascii title os host kernel pkgs wm uptime"
-export PF_COL1=5
-export PF_COL2=2
-export PF_COL3=7
-
 fish_aliases
-clear & neofetch
+clear & fastfetch
