07d8c08a8da7ed24490c4cb1263b784869f74c69
ByteSyntaxx <95436329+ByteSyntaxx@users.noreply.github.com>
2026-05-21 18:15:01 +0200

    Converted hyprland config from hyprlang to lua syntax

---
+++ config/hypr/themes/everforest.lua
@@ -0,0 +1,42 @@
+require("configs.animations")
+require("configs.keybinds")
+require("configs.keyboard")
+require("configs.misc")
+require("configs.monitors")
+require("configs.startup")
+
+
+-- decoration settings
+hl.config({
+    decoration = {
+        rounding       = 8,
+
+        shadow = {
+            enabled      = true,
+            range        = 10,
+            render_power = 100,
+            color        = "0xFF83C092",
+            color_inactive = "0xFF1E2326"
+        },
+
+        blur = {
+            enabled   = true,
+            size      = 8,
+            passes    = 3
+        },
+    }
+})
+
+-- general settings
+hl.config({
+    general = {
+        gaps_in = 4,
+        gaps_out = 5,
+        border_size = 3,
+        col = {
+            active_border = {colors={"0xFF83C092"}},
+            inactive_border = {colors={"0xFF1E2326"}}
+        },
+        layout = "master"
+    }
+})
