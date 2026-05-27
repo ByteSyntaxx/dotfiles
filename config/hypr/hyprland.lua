24bfbaf0ace548caeaee23954ae00ae7487a391c
ByteSyntaxx <95436329+ByteSyntaxx@users.noreply.github.com>
2026-05-27 11:15:07 +0200

    Updated Dotfiles 2026-05-27 11:15

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -1,1 +1,41 @@
-/home/bytesyntaxx/Documents/dotfiles/config/hypr/themes/catppuccin.lua
+require("modules.animations")
+require("modules.keybinds")
+require("modules.keyboard")
+require("modules.misc")
+require("modules.monitors")
+require("modules.startup")
+
+-- decoration settings
+hl.config({
+    decoration = {
+        rounding       = 8,
+
+        shadow = {
+            enabled      = true,
+            range        = 10,
+            render_power = 100,
+            color        = "0xFF89b4fa",
+            color_inactive = "0xFF181825"
+        },
+
+        blur = {
+            enabled   = true,
+            size      = 8,
+            passes    = 3
+        },
+    }
+})
+
+-- general settings
+hl.config({
+    general = {
+        gaps_in = 4,
+        gaps_out = 5,
+        border_size = 3,
+        col = {
+            active_border = {colors={"0xFF89b4fa"}},
+            inactive_border = {colors={"0xFF181825"}}
+        },
+        layout = "master"
+    }
+})
