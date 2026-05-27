129d8ae298cf17856e97d6ac3cc68f43e939fcce
ByteSyntaxx <95436329+ByteSyntaxx@users.noreply.github.com>
2026-05-27 11:13:21 +0200

    Moved hypr/configs to hypr/modules

---
+++ config/hypr/modules/monitors.lua
@@ -0,0 +1,5 @@
+hl.monitor({output="DP-1",mode="1920x1080@60",position="0x0",scale="1"})
+hl.monitor({output="HDMI-A-1",mode="1920x1080@60",position="1920x0",scale="1"})
+
+hl.workspace_rule({workspace="1",monitor="DP-1"})
+hl.workspace_rule({workspace="2",monitor="HDMI-A-1"})
