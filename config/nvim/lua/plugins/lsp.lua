657aa71fb12bc81ac60b333787fa16d507b5711a
ByteSyntaxx <95436329+ByteSyntaxx@users.noreply.github.com>
2026-06-02 17:24:10 +0200

    Enabled inline diagnostics; Added pylsp; Removed cssls, jsonjs and keybinds

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,26 +1,11 @@
-local on_attach = function(_,_)
-
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
-end
-
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
+    {"williamboman/mason.nvim",config = function()require("mason").setup()end},
     {
         "williamboman/mason-lspconfig.nvim",
         dependencies = { "williamboman/mason.nvim" }, -- Ensure Mason is loaded first
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls", -- Correct LSP name for Lua
-                },
+                ensure_installed = { "lua_ls", "pylsp" },
             })
         end,
     },
@@ -29,7 +14,7 @@ return {
         dependencies = { "williamboman/mason-lspconfig.nvim" }, -- Ensure LSP configurations are loaded after Mason
         config = function()
             local caps = require("cmp_nvim_lsp").default_capabilities()
-            vim.lsp.config['luals'] = {
+            vim.lsp.config['lua_ls'] = {
                 cmd = { 'lua-language-server' },
                 filetypes = { 'lua' },
                 root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
@@ -37,27 +22,15 @@ return {
                 settings = {
                     Lua = {
                         runtime = { version = 'LuaJIT' },
-                        diagnostics = { globals = { 'vim' } },
+                        diagnostics = { globals = { 'vim' , 'hl' } },
                         telemetry = { enable = false },
                     },
                 },
             }
-            vim.lsp.config['cssls'] = {
-                cmd = { 'vscode-css-language-server', '--stdio' },
-                filetypes = { 'css', 'scss', 'less' },
-                root_markers = { 'package.json', '.git' },
-                capabilities = caps,
-                settings = {
-                    css = { validate = true },
-                    scss = { validate = true },
-                    less = { validate = true },
-                },
-            }
-            vim.lsp.config['jsonls'] = {
-                cmd = { 'vscode-json-languageserver', '--stdio' },
-                filetypes = { 'json', 'jsonc' },
-                root_markers = { 'package.json', '.git', 'config.jsonc' },
-                capabilities = caps,
+            vim.lsp.config['pylsp'] = {
+                cmd = {'pylsp'},
+                filetypes = {'python'},
+                capabilities = caps
             }
             for name, _ in pairs(vim.lsp.config._configs) do
                 if name ~= '*' then
@@ -66,4 +39,5 @@ return {
             end
         end,
     },
+    vim.diagnostic.config({ virtual_text = true })
 }
