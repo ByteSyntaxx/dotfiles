d88fbc15d6c86e77396971151644417e5227c935
ByteSyntaxx <95436329+ByteSyntaxx@users.noreply.github.com>
2026-06-02 17:24:50 +0200

    Enabled tokyonight colorscheme

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,10 +1,13 @@
 return {
-    {'catppuccin/nvim'},
-    {'nvim-lualine/lualine.nvim', requires={'nvim-tree/nvim-web-devicons'},
-    config=function()
-        require('lualine').setup({
-            options={theme='palenight'}
-        })
-    end
+--    {'catppuccin/nvim',config=function()vim.cmd.colorscheme('catppuccin-mocha')end},
+    {'folke/tokyonight.nvim',config=function()vim.cmd.colorscheme('tokyonight-storm')end},
+--    {'EdenEast/nightfox.nvim',config=function()vim.cmd.colorscheme('nightfox')end},
+{
+        'nvim-lualine/lualine.nvim', requires={'nvim-tree/nvim-web-devicons'},
+        config=function()
+            require('lualine').setup({
+                options={theme='ayu_mirage'}
+            })
+        end
     }
 }
