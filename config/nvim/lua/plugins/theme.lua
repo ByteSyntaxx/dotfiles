5d92dc57e3c61fb0f9d97e9150eed71eea6dec29
ByteSyntaxx <95436329+ByteSyntaxx@users.noreply.github.com>
2026-06-05 09:20:41 +0200

    Fixed indentation

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -2,7 +2,7 @@ return {
 --    {'catppuccin/nvim',config=function()vim.cmd.colorscheme('catppuccin-mocha')end},
     {'folke/tokyonight.nvim',config=function()vim.cmd.colorscheme('tokyonight-storm')end},
 --    {'EdenEast/nightfox.nvim',config=function()vim.cmd.colorscheme('nightfox')end},
-{
+    {
         'nvim-lualine/lualine.nvim', requires={'nvim-tree/nvim-web-devicons'},
         config=function()
             require('lualine').setup({
