d2370311397cacce9354e9bb0cc5e55f7f392deb
ByteSyntaxx <95436329+ByteSyntaxx@users.noreply.github.com>
2026-07-19 17:51:34 +0200

    Updated neovim options

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,6 +1,8 @@
 vim.opt.expandtab=true
 vim.opt.number=true
-vim.opt.relativenumber=false
+vim.opt.relativenumber=true
 vim.opt.shiftwidth=4
 vim.opt.tabstop=4
 vim.opt.signcolumn="no"
+vim.opt.cursorline=true
+vim.opt.cursorcolumn=true
