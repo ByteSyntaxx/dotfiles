1c8559f751c633f7a53797dc031050a24b873a50
ByteSyntaxx <95436329+ByteSyntaxx@users.noreply.github.com>
2026-08-20 16:07:30 +0200

    Added new themes

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,21 +1,17 @@
 return {
-    --    {'catppuccin/nvim',config=function()vim.cmd.colorscheme('catppuccin-mocha')end},
-    --    {'folke/tokyonight.nvim',config=function()vim.cmd.colorscheme('tokyonight-storm')end},
-    --    {'EdenEast/nightfox.nvim',config=function()vim.cmd.colorscheme('nightfox')end},
-    {'navarasu/onedark.nvim',
-        config = function()
-            require('onedark').setup {
-                style = 'darker'
-            }
-            require('onedark').load()
-        end
-    },
+    --{'catppuccin/nvim',config=function()vim.cmd.colorscheme('catppuccin-mocha')end},
+    --{'folke/tokyonight.nvim',config=function()vim.cmd.colorscheme('tokyonight-storm')end},
+    --{'EdenEast/nightfox.nvim',config=function()vim.cmd.colorscheme('nightfox')end},
+    --{'navarasu/onedark.nvim',config = function ()require('onedark').load()end},
+    --{"drewtempelmeyer/palenight.vim",config = function()vim.cmd.colorscheme("palenight")end},
+    {'rebelot/kanagawa.nvim', config=function ()vim.cmd.colorscheme("kanagawa")end},
+    --{'tiagovla/tokyodark.nvim', config=function ()vim.cmd.colorscheme('tokyodark')end},
     {
-    'nvim-lualine/lualine.nvim', requires={'nvim-tree/nvim-web-devicons'},
-    config=function()
-        require('lualine').setup({
-            options={theme='ayu_mirage'}
-        })
-    end
+        'nvim-lualine/lualine.nvim', requires={'nvim-tree/nvim-web-devicons'},
+        config=function()
+            require('lualine').setup({
+                options={theme='palenight'}
+            })
+        end
     }
 }
