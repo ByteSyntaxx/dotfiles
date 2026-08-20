7d07a45c9a09a0ff1b898cbc5ba9e605e616401d
ByteSyntaxx <95436329+ByteSyntaxx@users.noreply.github.com>
2026-08-20 16:06:37 +0200

    Added autocmd to start treesitter

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,18 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
-	build = ':TSUpdate',
-	config = function()
-		require('nvim-treesitter').setup({
-			highlight = {enabled = true},
-			indent = {enabled = true},
-			autotage = {enabled = true},
-			ensure_installed = {"lua"},
-			auto_install = false,
-		})
-	end
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+        require('nvim-treesitter').setup({
+            highlight = {enabled = true},
+            indent = {enabled = true},
+            autotage = {enabled = true},
+            ensure_installed = {"lua", "c", "python"},
+            auto_install = true,
+        })
+        vim.api.nvim_create_autocmd("FileType", {
+            callback = function ()
+                pcall(vim.treesitter.start)
+            end,
+        })
+    end
 }
