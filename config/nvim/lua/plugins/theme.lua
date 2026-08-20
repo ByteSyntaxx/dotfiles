c8a357ca13dc2b36511b0ee67a073a78b1dd7b7e
ByteSyntaxx <95436329+ByteSyntaxx@users.noreply.github.com>
2026-08-20 12:06:16 +0200

    Added one dark theme

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,13 +1,21 @@
 return {
---    {'catppuccin/nvim',config=function()vim.cmd.colorscheme('catppuccin-mocha')end},
-    {'folke/tokyonight.nvim',config=function()vim.cmd.colorscheme('tokyonight-storm')end},
---    {'EdenEast/nightfox.nvim',config=function()vim.cmd.colorscheme('nightfox')end},
-    {
-        'nvim-lualine/lualine.nvim', requires={'nvim-tree/nvim-web-devicons'},
-        config=function()
-            require('lualine').setup({
-                options={theme='ayu_mirage'}
-            })
+    --    {'catppuccin/nvim',config=function()vim.cmd.colorscheme('catppuccin-mocha')end},
+    --    {'folke/tokyonight.nvim',config=function()vim.cmd.colorscheme('tokyonight-storm')end},
+    --    {'EdenEast/nightfox.nvim',config=function()vim.cmd.colorscheme('nightfox')end},
+    {'navarasu/onedark.nvim',
+        config = function()
+            require('onedark').setup {
+                style = 'darker'
+            }
+            require('onedark').load()
         end
+    },
+    {
+    'nvim-lualine/lualine.nvim', requires={'nvim-tree/nvim-web-devicons'},
+    config=function()
+        require('lualine').setup({
+            options={theme='ayu_mirage'}
+        })
+    end
     }
 }
